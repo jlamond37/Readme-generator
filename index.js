81007fac291d4914e271d0ef159adc922909c2e6
jlamond37 <jlamond37@gmail.com>
2022-08-10 20:57:09 -0400

    added more questions, edited functions

--- index.js
+++ index.js
@@ -2,7 +2,32 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [
+const generateMD = ({username, repository, title, description, installation, usage, credit, license}) =>
+`# ${title}
+
+## Description
+${description}
+
+
+## Installation
+${installation}
+
+
+## Usage
+${usage}
+
+
+## Credits
+${credit}
+
+## License
+${license}
+
+## Questions
+${username} ${githubprofile}
+`;
+inquirer
+    .prompt ([
     {
         type: 'input',
         name: 'username',
@@ -11,8 +36,8 @@ const questions = [
     },
     {
         type: 'input',
-        name: 'repository',
-        message: 'Enter your repo name',
+        name: 'githubprofile',
+        message: 'Please provide a link to your github profile',
     },
     {
         type: 'input',
@@ -47,11 +72,13 @@ const questions = [
         choices: ['afl-3.0', 'apache-2.0', 'artistic-2.0', 'bsl-1.0', 'bsd-2-clause', 'bsd-3-clause', 'bsd-3-clause-clear', 'cc', 'cc0-1.0', 'cc-by-4.0', 'cc-by-sa-4.0', 'wtfpl', 'ecl-2.0', 'epl-1.0', 'epl-2.0', 'eupl-1.1', 'agpl-3.0', 'gpl', 'gpl-2.0', 'gpl-3.0', 'lgpl', 'lgpl-2.1', 'lgpl-3.0', 'isc', 'lppl-1.3c', 'ms-pl', 'mit', 'mpl-2.0', 'osl-3.0', 'postgresql', 'ofl-1.1', 'ncsa', 'unlicense', 'zlib']
     },
 
-];
+]);
+
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, err => {
+    fs.writeFile('README.md', mdPageContent, err => {
         if (err) {
             return console.log(err);
         }
